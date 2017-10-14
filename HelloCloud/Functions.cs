d22c5fbc5e5b9aa99da5703a229b18550c9ee3c6
Sohel Shekha <shekha.sohel@gmail.com>
2017-10-14 13:54:40 +0800

    Getting Started with Azure Functions

---
+++ HelloCloud/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Linq;
+
+namespace HelloCloud
+{
+    public static class Functions
+    {
+        [FunctionName("Hello")]
+        public static string Hello([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+                                   HttpRequestMessage req, TraceWriter log)
+        {
+            var name = req.GetQueryNameValuePairs().First(n => n.Key == "name").Value;
+            return $"Hello {name}!";
+        }
+    }
+}
